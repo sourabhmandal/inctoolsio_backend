d41e766eb75a1cc652fcc19db3810f69ff3a1820
sourabhmandal <19mandal97@gmail.com>
2022-03-23 23:18:14 +0530

    add redirect url to env

--- src/auth/google.startegy.ts
+++ src/auth/google.startegy.ts
@@ -9,7 +9,7 @@ export class GoogleStategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env['GOOGLE_CLIENT_ID'],
       clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-      callbackURL: process.env['GOOGLE_CALLBACK_URL'],
+      callbackURL: `${process.env['BACKEND_BASE_URL']}/auth/google/callback`,
       scope: ['email', 'profile'],
     }); // config
   }
